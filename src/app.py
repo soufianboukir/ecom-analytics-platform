f4f3712654fe841300cc511e45d1a2f2b34c3fd8
soufian <soufianeboukir0@gmail.com>
2026-05-06 23:42:25 +0100

    fix: EDA-first 2 vis

--- src/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# This code runs whenever the app starts or a user interacts with a widget
-st.title("Hello Streamlit!")
-st.write("This is my basic entry point script.")
-
-name = st.text_input("Enter your name")
-if name:
-    st.write(f"Hello, {name}!")
